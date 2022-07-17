76fa9b1b64bb68336e1f2c6bbca367e2ad297c19
Guicbdiniz <guicbdeniz@hotmail.com>
2022-07-16 22:57:25 -0300

    feat: create API type to hold more that mux logic

--- rest-api/src/api/api.go
+++ rest-api/src/api/api.go
@@ -6,10 +6,30 @@ import (
 	"github.com/Guicbdiniz/go-projects/rest-api/handlers/ping"
 )
 
-func CreateAPI() *http.ServeMux {
-	api := http.NewServeMux()
+type API struct {
+	serveMux *http.ServeMux
+}
+
+// Register the handler function for the given pattern in the ServeMux.
+func (api *API) HandleFunc(pattern string, handler func(http.ResponseWriter, *http.Request)) {
+	api.serveMux.HandleFunc(pattern, handler)
+}
+
+// Register the handler for the given pattern in the ServeMux.
+func (api *API) Handle(pattern string, handler http.Handler) {
+	api.serveMux.Handle(pattern, handler)
+}
+
+// Dispatch the request to the handler whose pattern most closely matches the request URL.
+func (api *API) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	api.serveMux.ServeHTTP(w, r)
+}
+
+func CreateAPI() *API {
+	api := API{}
+	api.serveMux = http.NewServeMux()
 
 	api.HandleFunc("/ping", ping.Ping)
 
-	return api
+	return &api
 }
--- rest-api/src/api/api_test.go
+++ rest-api/src/api/api_test.go
@@ -1,7 +1,6 @@
 package api
 
 import (
-	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -20,13 +19,6 @@ func TestCreateAPI(t *testing.T) {
 	if response.StatusCode != http.StatusOK {
 		t.Fatalf("Ping request did not return the correct status code. Expected %d, got %d",
 			http.StatusAccepted, response.StatusCode)
-	}
-
-	responseBody, _ := io.ReadAll(response.Body)
-
-	if string(responseBody) != "pong" {
-		t.Fatalf("Ping request did not return the correct body. Expected %s, got %s",
-			"pong", string(responseBody))
 	}
 
 }
