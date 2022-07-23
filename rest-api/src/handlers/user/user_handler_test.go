d22d6437cd8a399c7240a30f31e99ac5845c8a3b
Guicbdiniz <guicbdeniz@hotmail.com>
2022-07-23 14:14:55 -0300

    test: create setUp and tearDown functions for userHandler test

--- rest-api/src/handlers/user/user_handler_test.go
+++ rest-api/src/handlers/user/user_handler_test.go
@@ -7,23 +7,19 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/Guicbdiniz/go-projects/rest-api/db/queries"
-
-	"github.com/Guicbdiniz/go-projects/rest-api/models"
-
 	"github.com/Guicbdiniz/go-projects/rest-api/api"
+	"github.com/Guicbdiniz/go-projects/rest-api/db/queries"
+	"github.com/Guicbdiniz/go-projects/rest-api/models"
 	"github.com/Guicbdiniz/go-projects/rest-api/utils"
 
 	_ "github.com/lib/pq"
 )
 
-func TestUserHandler(t *testing.T) {
-	request := httptest.NewRequest("GET", "/user", nil)
-	responseRecorder := httptest.NewRecorder()
+func setUpTestDBForUserHandler(t *testing.T) *sql.DB {
+	t.Log("set up test case")
 
 	db, err := sql.Open("postgres", api.DatabaseTestingUrl)
 	utils.CheckTestError(t, err, "Error captured while creating a db")
-	defer db.Close()
 
 	err = queries.CreateUsersTable(db)
 	utils.CheckTestError(t, err, "Error captured while creating users table")
@@ -35,6 +31,26 @@ func TestUserHandler(t *testing.T) {
 	}
 	err = initialUser.SaveUser(db)
 	utils.CheckTestError(t, err, "Error captured while creating initial user")
+
+	return db
+}
+
+func tearDownUserHandlerTestCase(t *testing.T, db *sql.DB) {
+	t.Log("teardown test case")
+
+	err := queries.DropUsersTable(db)
+	utils.CheckTestError(t, err, "Error captured while droping user table")
+
+	err = db.Close()
+	utils.CheckTestError(t, err, "Error captured while closing db connection")
+}
+
+func TestUserHandler(t *testing.T) {
+	db := setUpTestDBForUserHandler(t)
+	defer tearDownUserHandlerTestCase(t, db)
+
+	request := httptest.NewRequest("GET", "/user", nil)
+	responseRecorder := httptest.NewRecorder()
 
 	handler := CreateUserHandler(db)
 	handler.ServeHTTP(responseRecorder, request)
@@ -51,6 +67,4 @@ func TestUserHandler(t *testing.T) {
 	utils.AssertEqual(t, 1, len(jsonBody.Data), "GET request to /user did not return correct body")
 	utils.AssertEqual(t, jsonBody.ErrorText, "", "GET request to /user did not return correct error text")
 	utils.AssertEqual(t, jsonBody.Data[0].Username, "guidi", "GET request to /user did not return correct body")
-
-	queries.DropUsersTable(db)
 }
